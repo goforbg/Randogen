bff72f4ef88e5563208e1559507b1730b41a94a4
BG XPS <techydevil@gmail.com>
2020-01-06 15:32:22 +0530

    UI changes

--- app/src/main/java/com/androar/randogen/MainActivity.kt
+++ app/src/main/java/com/androar/randogen/MainActivity.kt
@@ -3,6 +3,7 @@ package com.androar.randogen
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -25,6 +26,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         val arrayList = generateEpisodes()
         val randomButton = findViewById(R.id.randombutton) as TextView
