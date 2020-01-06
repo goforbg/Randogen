b7b9be5b50efffaf41340167d07a4c4e167ef570
BG XPS <techydevil@gmail.com>
2020-01-06 11:41:33 +0530

    Added logo

--- app/src/main/java/com/androar/randogen/MainActivity.kt
+++ app/src/main/java/com/androar/randogen/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.androar.randogen
 
-import android.media.Image
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         randomButton.setOnClickListener {
             val r = Random()
-            var random = r. nextInt(7 - 1) + 1;
+            var random = r. nextInt(10 - 1) + 1;
 
             Glide.with(this)
                     .load(arrayList.get(random).i)
@@ -40,7 +42,13 @@ class MainActivity : AppCompatActivity() {
             episodeImage.scaleType = ImageView.ScaleType.CENTER_CROP
             episodeTitle.setText(arrayList.get(random).t)
 
+            episodeImage.setOnClickListener{
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/results?search_query="+arrayList.get(random).t))
+                startActivity(intent)
+            }
         }
+
+
 
     }
 
